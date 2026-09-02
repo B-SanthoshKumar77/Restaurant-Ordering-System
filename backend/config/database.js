d7366c248ddfdc58baa78c33c4f0e692bdc28742
Sanju <b.santhoshkumarbsk77@gmail.com>
2026-09-02 19:24:14 +0530

    db update

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,23 +1,29 @@
 import mysql from "mysql2";
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.TIDB_HOST,
   port: process.env.TIDB_PORT || 4000,
   user: process.env.TIDB_USER,
   password: process.env.TIDB_PASSWORD,
   database: process.env.TIDB_DATABASE || "qfood",
+
   ssl: {
     minVersion: "TLSv1.2",
   },
+
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
-db.connect((error) => {
+db.getConnection((error, connection) => {
   if (error) {
     console.error("Database connection failed:", error);
     return;
   }
 
   console.log("Successfully connected to the database.");
+  connection.release();
 });
 
 export default db;
